b8f71e4a25a6474042c77b828fb87bc4b9b364d7
Chris Gilmer <chris.gilmer@gmail.com>
2016-06-03 16:54:12 -0700

    Update ansilary files for ansible to work

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #! /bin/bash -e
 
+echo
 echo "Install ansible and required python requirements"
 pip install -r requirements/python.txt
 
+echo
 echo "Install ansible galaxy requirements"
 ansible-galaxy install -r requirements/galaxy.yml -p galaxy/
+
+echo
+echo "Touch the Ansible Vault secrets file in ~/.vault_pass_local.txt"
+echo "You need to copy the password from 1password into this file"
+touch ~/.vault_pass_local.txt
+
+if [ ! -d ~/.ssh/keys ] && [ ! -L ~/.ssh/keys ]; then
+    echo
+    echo "Create a symlink to ~/.ssh/keys -> ~/.ssh"
+    ln -s ~/.ssh/keys ~/.ssh
+fi
+echo
