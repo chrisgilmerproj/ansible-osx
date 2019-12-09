5f2c33040d93c6f616def22932a715f8e42f40e3
Chris Gilmer <chris@truss.works>
2019-12-09 11:56:21 -0800

    Update python versions

--- install_framework_python.sh
+++ install_framework_python.sh
@@ -1,17 +1,16 @@
-#! /bin/bash -ex
+#! /usr/bin/env bash
 
-mkdir -p $(pyenv root)/versions/2.7.12
+set -eux -o pipefail
 
-CFLAGS="-I/usr/local/opt/readline/include -I/usr/local/opt/openssl/include" \
-LDFLAGS="-L/usr/local/opt/readline/lib -L/usr/local/opt/openssl/lib" \
+# brew install readline xz openssl
+
+PYENV_ROOT="$(pyenv root)"
+# VERSION=2.7.17
+VERSION=3.8.0
+mkdir -p "${PYENV_ROOT}/versions/${VERSION}"
+
+CFLAGS="-I$(brew --prefix readline)/include -I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" \
+LDFLAGS="-L$(brew --prefix readline)/lib -L$(brew --prefix openssl)/lib" \
 CC="clang" \
-PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs2 --enable-framework=$(pyenv root)/versions/2.7.12/" \
-pyenv install -f 2.7.12
-
-# pyenv shell 2.7.12
-# pip install numpy scipy matplotlib
-# python -c "import matplotlib.pyplot as plt"
-
-# CFLAGS="-I/usr/local/opt/readline/include -I/usr/local/opt/openssl/include -I$(xcrun --show-sdk-path)/usr/include" \
-# LDFLAGS="-L/usr/local/opt/readline/lib -L/usr/local/opt/openssl/lib -L$(xcrun --show-sdk-path)/usr/lib" \
-# PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs2 --enable-framework CC=clang" \
+PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs2 --enable-framework=${PYENV_ROOT}/versions/${VERSION}/" \
+pyenv install -f "${VERSION}"
